aa3a322376cad396f718a84227130e1255b3df48
草札 <8866629@qq.com>
2020-06-19 11:51:18 +0800

    1.6.0

--- Src/public/static/admin/caozha/js/all.js
+++ Src/public/static/admin/caozha/js/all.js
@@ -13,9 +13,14 @@ var layer_skin_color=new Array(
     "#e82121",
     "#963885");
 
+var layuimini_bgColorDefault=3; // 主题默认配置
 
 function layer_skin(layer) {
     var bgcolorId = sessionStorage.getItem('layuiminiBgcolorId');
+    if (bgcolorId === null || bgcolorId === undefined || bgcolorId === '') {
+        bgcolorId = layuimini_bgColorDefault;
+        sessionStorage.setItem('layuiminiBgcolorId', bgcolorId);
+    }
     $('<style>').html('body .caozha-layer-'+bgcolorId+' .layui-layer-title{background:'+layer_skin_color[bgcolorId]+';}\n' +
         'body .caozha-layer-'+bgcolorId+' .layui-layer-btn0{border-color: '+layer_skin_color[bgcolorId]+';}\n' +
         'body .caozha-layer-'+bgcolorId+' .layui-layer-btn a{background:'+layer_skin_color[bgcolorId]+';}\n' +
