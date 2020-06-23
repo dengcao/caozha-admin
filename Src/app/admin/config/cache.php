e1d3227937c647b8399a47e6cd75e832cf6a7bf8
草札 <8866629@qq.com>
2020-06-23 21:38:27 +0800

    1.6.0

--- Src/app/admin/config/cache.php
+++ Src/app/admin/config/cache.php
@@ -14,7 +14,7 @@ return [
             // 驱动方式
             'type'       => 'File',
             // 缓存保存目录
-            'path'       => '',
+            'path'       => 'cache',
             // 缓存前缀
             'prefix'     => 'caozha',
             // 缓存有效期 0表示永久缓存
